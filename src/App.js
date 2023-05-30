61a9109247dabc021f1c1f22c579727f8f78831c
Haruuth <haruuth@gmail.com>
2023-05-30 13:34:54 +0200

    mesas con seleccion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Cervezas } from "./Components/Cervezas/Cervezas";
 import { Ginebras } from "./Components/Ginebras/Ginebras";
 import { Categorias } from "./Components/Categorias/Categorias";
@@ -10,39 +10,65 @@ import { Cocteles } from "./Components/Cocteles/Cocteles";
 function App() {
   const [selectedProduct, setSelectedProduct] = useState([]);
   const [salonOpen, setSalonOpen] = useState(false);
+  const [selectedMesaID, setSelectedMesaID] = useState("");
+  const [selectedMesaInfo, setSelectedMesaInfo] = useState({});
 
 
-  const openSalones = () => {
-    setSalonOpen(prevState => !prevState);
+  const handleMesaSelection = (numeroMesa) => {
+    setSelectedMesaID(numeroMesa);
+    setSelectedProduct(selectedMesaInfo[numeroMesa] || []);
   };
 
+  const updateSelectedMesaInfo = () => {
+    setSelectedMesaInfo((prevState) => ({
+      ...prevState,
+      [selectedMesaID]: selectedProduct,
+    }));
+  };
 
-  const totalCuenta = selectedProduct.reduce(
-    (sum, product) => sum + product.precio,
-    0
-  ).toFixed(2);
+  useEffect(() => {
+    updateSelectedMesaInfo();
+  }, [selectedProduct, ]);
 
-  
+  const openSalones = () => {
+    setSalonOpen((prevState) => !prevState);
+  };
+
+  const totalCuenta = selectedProduct
+    .reduce((sum, product) => sum + product.precio, 0)
+    .toFixed(2);
+
   const quitProducts = (index) => {
-    const updateProducts = [...selectedProduct];
-    updateProducts.splice(index, 1 )
-    setSelectedProduct(updateProducts);
-  }
- 
+    const updatedProducts = [...selectedProduct];
+    updatedProducts.splice(index, 1);
+    setSelectedProduct(updatedProducts);
+  };
+
 
   return (
     <Router>
       <div className="containerApp">
         <div className="App">
           {/* <div className="modal"> */}
-          <Salon isOpen={salonOpen} onClose={() => setSalonOpen(false)}></Salon>
+          <Salon
+            onMesaSelect={handleMesaSelection}
+            isOpen={salonOpen}
+            onClose={() => setSalonOpen(false)}
+            selectedMesaInfo={selectedMesaInfo}
+          ></Salon>
+        
           {/* </div> */}
 
           <div className="textarea">
             <div className="textarea--bloque">
               {/* <p>Cuenta total:</p> */}
               {selectedProduct.map((product, index) => (
-                 <div className={`cuenta ${product.seleccionado ? 'seleccionado' : ''}`} key={index}>
+                <div
+                  className={`cuenta ${
+                    product.seleccionado ? "seleccionado" : ""
+                  }`}
+                  key={index}
+                >
                   <p>{product.nombre}</p>
                   <p>{product.precio}</p>
                   <button onClick={() => quitProducts(index)}>❌</button>
@@ -50,9 +76,9 @@ function App() {
               ))}
             </div>
             <div className="barraInferior">
-              <p className="mesa">
+              <p className="mesa1">
                 <strong>Mesa: </strong>
-                <strong> ID </strong>
+                <strong> {selectedMesaID} </strong>
               </p>
               <p className="total">
                 <strong>Total: </strong>
@@ -60,7 +86,6 @@ function App() {
               </p>
             </div>
           </div>
-
           <div className="calculadora">
             <p> CALCULADORA </p>
           </div>
@@ -85,9 +110,10 @@ function App() {
         </div>
         <div className="botonesLaterales">
           <p>BOTONES LATERALES</p>
-          <button onClick={openSalones} className="mesas">MESAS</button>
+          <button onClick={openSalones} className="mesas">
+            MESAS
+          </button>
           <button onClick={() => setSelectedProduct([])}>ANULAR CUENTA</button>
-
         </div>
       </div>
     </Router>
--- src/Components/Salon/Salon.jsx
+++ src/Components/Salon/Salon.jsx
@@ -1,41 +1,16 @@
 import React, { useState } from "react";
-
 import "./Salon.scss";
-// import Draggable from "react-draggable";
 
-
-const Salon = ({ isOpen }) => {
+const Salon = ({ isOpen, onClose, onMesaSelect, selectedMesaInfo }) => {
   const [diseñoSalon, setDiseñoSalon] = useState(true);
   const [mostrarNuevaMesa, setMostrarNuevaMesa] = useState(false);
-//   const [mesas, setMesas] = useState([]);
+  const [mesaSeleccionada] = useState(null);
+  const [setMesaInfo] = useState({});
 
-
-//FUNCION PARA GENERAR MESAS
-//   const generarMesas = () => {
-//     const nuevasMesas = [];
-//     const totalMesas = 17;
-
-//     for (let i = 1; i <= totalMesas; i++) {
-//       const mesa = {
-//         id: i,
-//         numero: i,
-//         nombre: `Mesa ${i}`,
-//         seleccionada: false,
-//         estado: "Libre",
-//         pedidos: [],
-//         cuenta: 0,
-        // Otras propiedades relevantes a tu caso
-//       };
-//       nuevasMesas.push(mesa);
-//     }
-
-//     setMesas(nuevasMesas);
-//   };
-
-//   useEffect(() => {
-//     generarMesas();
-//   }, []);
-
+  const handleMesaSelection = (numeroMesa) => {
+    onMesaSelect(numeroMesa);
+    onClose(); // Cerrar el componente "Salon" después de seleccionar la mesa
+  };
 
   if (!isOpen) {
     return null;
@@ -55,20 +30,13 @@ const Salon = ({ isOpen }) => {
     setMostrarNuevaMesa(!mostrarNuevaMesa);
   };
 
-  // const NuevaMesa = () => {
-  //   const handleDrag = (e, data) => {
-      
-      // Manejar el evento de arrastre de la nueva mesa
-  //   };
-
-  //   return (
-  //     <Draggable onStop={handleDrag}>
-  //       <div className="mesa nueva-mesa">Nueva Mesa</div>
-  //     </Draggable>
-  //   );
-  // };
-
-
+  const guardarMesaSeleccionada = () => {
+    setMesaInfo((prevState) => ({
+      ...prevState,
+      [mesaSeleccionada]: [],
+    }));
+    onClose(); // Cerrar el componente "Salon" después de seleccionar la mesa
+  };
 
   return (
     <div className="contenedor">
@@ -87,95 +55,329 @@ const Salon = ({ isOpen }) => {
         {diseñoSalon ? (
           <div className="diseñoDeSalon">
             <div className="mitad">
+              <div
+                id="1"
+                className={`mesa ${
+                  mesaSeleccionada === "1" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["1"] && selectedMesaInfo["1"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("1")}
+              >
+                Mesa 1
+              </div>
 
-              {/* <Draggable> */}
-                <div id="Mesa9" className="mesa">
-                  Mesa9
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable>{mostrarNuevaMesa && <NuevaMesa />}</Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa10" className="mesa">
-                  Mesa10
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa11" className="mesa">
-                  Mesa11
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Barr1" className="mesa">
-                  Barr1
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Barr2" className="mesa">
-                  Barr2
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Barr3" className="mesa">
-                  Barr3
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Barr4" className="mesa">
-                  Barr4
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Barr5" className="mesa">
-                  Barr5
-                </div>
-              {/* </Draggable> */}
+              <div
+                id="2"
+                className={`mesa ${
+                  mesaSeleccionada === "2" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["2"] && selectedMesaInfo["2"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("2")}
+              >
+                Mesa 2
+              </div>
+              <div
+                id="3"
+                className={`mesa ${
+                  mesaSeleccionada === "3" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["3"] && selectedMesaInfo["3"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("3")}
+              >
+                Mesa 3
+              </div>
+              <div
+                id="4"
+                className={`mesa ${
+                  mesaSeleccionada === "4" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["4"] && selectedMesaInfo["4"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("4")}
+              >
+                Mesa 4
+              </div>
+              <div
+                id="5"
+                className={`mesa ${
+                  mesaSeleccionada === "5" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["5"] && selectedMesaInfo["5"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("5")}
+              >
+                Mesa 5
+              </div>
+              <div
+                id="6"
+                className={`mesa ${
+                  mesaSeleccionada === "6" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["6"] && selectedMesaInfo["6"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("6")}
+              >
+                Mesa 6
+              </div>
+              <div
+                id="7"
+                className={`mesa ${
+                  mesaSeleccionada === "7" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["7"] && selectedMesaInfo["7"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("7")}
+              >
+                Mesa 7
+              </div>
+              <div
+                id="8"
+                className={`mesa ${
+                  mesaSeleccionada === "8" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["8"] && selectedMesaInfo["8"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("8")}
+              >
+                Mesa 8
+              </div>
             </div>
             <div className="mitad">
-              {/* <Draggable> */}
-                <div id="Mesa8" className="mesa">
-                  Mesa8
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa6" className="mesa">
-                  Mesa6
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa5" className="mesa">
-                  Mesa5
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa12" className="mesa">
-                  Mesa12
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa4" className="mesa">
-                  Mesa4
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa3" className="mesa">
-                  Mesa3
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa2" className="mesa">
-                  Mesa2
-                </div>
-              {/* </Draggable> */}
-              {/* <Draggable> */}
-                <div id="Mesa1" className="mesa">
-                  Mesa1
-                </div>
-              {/* </Draggable> */}
-
+              <div
+                id="9"
+                className={`mesa ${
+                  mesaSeleccionada === "9" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["9"] && selectedMesaInfo["9"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("9")}
+              >
+                Mesa 9
+              </div>
+              <div
+                id="10"
+                className={`mesa ${
+                  mesaSeleccionada === "10" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["10"] && selectedMesaInfo["10"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("10")}
+              >
+                Mesa 10
+              </div>
+              <div
+                id="11"
+                className={`mesa ${
+                  mesaSeleccionada === "11" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["11"] && selectedMesaInfo["11"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("11")}
+              >
+                Mesa 11
+              </div>
+              <div
+                id="12"
+                className={`mesa ${
+                  mesaSeleccionada === "12" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["12"] && selectedMesaInfo["12"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("12")}
+              >
+                Mesa 12
+              </div>
+              <div
+                id="B1"
+                className={`mesa ${
+                  mesaSeleccionada === "B1" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["B1"] && selectedMesaInfo["B1"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("B1")}
+              >
+                Mesa B1
+              </div>
+              <div
+                id="B2"
+                className={`mesa ${
+                  mesaSeleccionada === "B2" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["B2"] && selectedMesaInfo["B2"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("B2")}
+              >
+                Mesa B2
+              </div>
+              <div
+                id="B3"
+                className={`mesa ${
+                  mesaSeleccionada === "B3" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["B3"] && selectedMesaInfo["B3"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("B3")}
+              >
+                Mesa B3
+              </div>
+              <div
+                id="B4"
+                className={`mesa ${
+                  mesaSeleccionada === "B4" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["B4"] && selectedMesaInfo["B4"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("B4")}
+              >
+                Mesa B4
+              </div>
+              <div
+                id="B5"
+                className={`mesa ${
+                  mesaSeleccionada === "B5" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["B5"] && selectedMesaInfo["B5"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("B5")}
+              >
+                Mesa B5
+              </div>
             </div>
           </div>
         ) : (
-          <div className="diseñoDeTerraza">AQUI VA LA TERRAZA</div>
+          <div className="diseñoDeTerraza">
+            <div className="mitad">
+              <div
+                id="T1"
+                className={`mesa ${
+                  mesaSeleccionada === "T1" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["T1"] && selectedMesaInfo["T1"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("T1")}
+              >
+                Mesa T1
+              </div>
+
+              <div
+                id="T2"
+                className={`mesa ${
+                  mesaSeleccionada === "T2" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["T2"] && selectedMesaInfo["T2"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("T2")}
+              >
+                Mesa T2
+              </div>
+              <div
+                id="T3"
+                className={`mesa ${
+                  mesaSeleccionada === "T3" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["T3"] && selectedMesaInfo["T3"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("T3")}
+              >
+                Mesa T3
+              </div>
+              <div
+                id="T4"
+                className={`mesa ${
+                  mesaSeleccionada === "T4" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["T4"] && selectedMesaInfo["T4"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("T4")}
+              >
+                Mesa T4
+              </div>
+              <div
+                id="T5"
+                className={`mesa ${
+                  mesaSeleccionada === "T5" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["T5"] && selectedMesaInfo["T5"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("T5")}
+              >
+                Mesa T5
+              </div>
+              <div
+                id="T6"
+                className={`mesa ${
+                  mesaSeleccionada === "T6" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["T6"] && selectedMesaInfo["T6"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("T6")}
+              >
+                Mesa T6
+              </div>
+              <div
+                id="T7"
+                className={`mesa ${
+                  mesaSeleccionada === "T7" ? "seleccionada" : ""
+                } ${
+                  selectedMesaInfo["T7"] && selectedMesaInfo["T7"].length > 0
+                    ? "con-informacion"
+                    : ""
+                }`}
+                onClick={() => handleMesaSelection("T7")}
+              >
+                Mesa T7
+              </div>
+            </div>
+          </div>
         )}
       </div>
     </div>
